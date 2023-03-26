8909d50c8328a59c2f3bf26b496dc2a1f48e0b55
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-03-26 12:08:44 -0400

    updated to correctly use getters and setters

--- RoadBuilder.java
+++ RoadBuilder.java
@@ -3,44 +3,46 @@ import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.ArrayList;
 import java.util.*;
+import javax.swing.*;
 
 public class RoadBuilder extends Grid {
-    
     // bfs
-   public static void bfs_pathfinding(Grid grid, Tile start, Tile goal) {
-    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // array of direction vectors
-    int m = grid.rows(); // number of rows
-    int n = grid.cols(); // number of columns
-    int[][] parent = new int[m][n]; // array to keep track of the parent of each visited node
-    boolean[][] visited = new boolean[m][n]; // array to keep track of visited nodes
-    ArrayList<Tile> queue = new ArrayList<>(); // ArrayList to hold nodes to be processed
-    queue.add(start); // add starting tile to queue
-    visited[start.row()][start.col()] = true; // mark starting tile as visited
-    int index = 0; // index of the current node being processed
-    while (index < queue.size()) {
-        Tile current = queue.get(index);
-        index++;
-        if (current == goal) {
-            // reconstruct path and mark it on the grid
-            while (current != start) {
-                grid.getTile(current.row(), current.col()).set_path(true); // mark tile as part of path
-                current = grid.getTile(parent[current.row()][current.col()]); // get parent tile
+    public static void bfs_pathfinding(Grid grid, Tile start, Tile goal) {
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // array of direction vectors
+        int m = grid.GetRows(); // number of rows
+        int n = grid.GetColumns(); // number of columns
+        int[][] parent = new int[m][n]; // array to keep track of the parent of each visited node
+        boolean[][] visited = new boolean[m][n]; // array to keep track of visited nodes
+        ArrayList<Tile> queue = new ArrayList<>(); // ArrayList to hold nodes to be processed
+        queue.add(start); // add starting tile to queue
+        visited[start.GetRow()][start.GetColumn()] = true; // mark starting tile as visited
+        int index = 0; // index of the current node being processed
+        
+        while (index < queue.size()) {
+            Tile current = queue.get(index);
+            index++;
+            if (current == goal) {
+                // reconstruct path and mark it on the grid
+                while (current != start) {
+                    grid.getTile(current.row(), current.col()).set_path(true); // mark tile as part of path
+                    current = grid.getTile(parent[current.row()][current.col()]); // get parent tile
+                }
+                grid.getTile(start.row(), start.col()).set_path(true); // mark starting tile as part of path
+                return;
             }
-            grid.getTile(start.row(), start.col()).set_path(true); // mark starting tile as part of path
-            return;
-        }
-        for (int[] dir : directions) {
-            int nx = current.row() + dir[0];
-            int ny = current.col() + dir[1];
-            Tile neighbor = grid.getTile(nx, ny);
-            if (neighbor != null && neighbor.weight() == 1 && !visited[nx][ny]) {
-                queue.add(neighbor);
-                visited[nx][ny] = true;
-                parent[nx][ny] = current.index();
+            
+            for (int[] dir : directions) {
+                int nx = current.row() + dir[0];
+                int ny = current.col() + dir[1];
+                Tile neighbor = grid.getTile(nx, ny);
+                if (neighbor != null && neighbor.weight() == 1 && !visited[nx][ny]) {
+                    queue.add(neighbor);
+                    visited[nx][ny] = true;
+                    parent[nx][ny] = current.index();
+                }
             }
         }
     }
-}
 
     // A*
     public static void aStar_pathfinding(Grid grid, Tile start, Tile goal) {
@@ -135,11 +137,14 @@ public class RoadBuilder extends Grid {
 
 
     // prints grid
-    static void visualize() {
+    static void visualize(Grid grid) {
+        JFrame frame = new JFrame();
         
+        frame.setLayout("GridLayout");
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        
     }
 }
