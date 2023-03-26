b0e614a1d60c000f00a8b277ed0c7d4a5dff828c
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-03-26 12:07:59 -0400

    added example getters and setters

--- Grid.java
+++ Grid.java
@@ -1,23 +1,30 @@
 public class Grid {
     public static Tile start;
-
+    public static int rows;
+    public static int columns;
 
     public static Tile[] neighbors() {
         return null;
     }
 
+    public void set_goal() {
+
+    }
+
     public class Tile {
-
-        public Tile(int x_position, int y_position, boolean is_goal, boolean is_obstacle) {
-
-        }
-
-        public void set_goal() {
+        public static int row;
+        public static int column;
+        public Tile(int row, int column, boolean is_goal, boolean is_obstacle) {
 
         }
 
-        public static Tile[] neighbors() {
-            return null;
+        public int getRow() {
+            return row;
         }
+        public int getColumn() {
+            return row;
+        }
+
+        
     }
 }
