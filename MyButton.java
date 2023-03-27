12e1d73fedbd03004ee05e595bf988ba05aa12ad
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-03-27 15:48:01 -0400

    JButton override

---
+++ MyButton.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+// original code: https://github.com/kdeloach/labs/blob/master/java/yahtzee/src/Dice.java
+public class MyButton extends JButton implements MouseListener {
+    private boolean mousePressed = false;
+    private ArrayList<ActionListener> listeners = new ArrayList<ActionListener>();
+
+    public MyButton() {
+        super();
+
+        enableInputMethods(true);   
+        addMouseListener(this);
+    }
+
+    // @Override
+    // public void paintComponent(Graphics g) {
+
+    // }
+
+    public void mouseClicked(MouseEvent e){}
+    public void mouseEntered(MouseEvent e) {setCursor(new Cursor(Cursor.HAND_CURSOR));}
+    public void mouseExited(MouseEvent e) {setCursor(new Cursor(Cursor.DEFAULT_CURSOR));}
+    public void mousePressed(MouseEvent e)
+    {
+        notifyListeners(e);
+        mousePressed = true;
+        repaint();
+    }
+    public void mouseReleased(MouseEvent e)
+    {
+        mousePressed = false;
+    }
+    public void addActionListener(ActionListener listener)
+    {
+        listeners.add(listener);
+    }
+
+    private void notifyListeners(MouseEvent e)
+    {
+        ActionEvent event = new ActionEvent(this, ActionEvent.ACTION_PERFORMED, new String(), e.getWhen(), e.getModifiers());
+        synchronized(listeners)
+        {
+            for (int i = 0; i < listeners.size(); i++)
+            {
+                ActionListener tmp = listeners.get(i);
+                tmp.actionPerformed(event);
+            }
+        }
+    }
+}
