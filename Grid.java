fd56be7e033ff8738b6f6f7678baaab5e1f54e3a
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-03-27 19:11:02 -0400

    added comments for needed functions

--- Grid.java
+++ Grid.java
@@ -1,3 +1,18 @@
+/*  
+FOR KENNA:
+What we need:
+
+Grid: (either function or variable with getters and setters)
+rows (int), columns (int), start (Tile), tileArray (Tile[][]), 
+neighbors (Tile[])
+
+Tile: (either function or variable with getters and setters)
+row (int), column (int), isGoal (bool), isObstacle (bool)
+
+these are just the ones I can think of, feel free to add others you need
+but follow the same naming convention so they are easy to access
+*/
+
 public class Grid {
     public static Tile start;
     public static int rows;
