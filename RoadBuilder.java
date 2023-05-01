39a4e1a8986d42c1600dd5c231b40fccaaceabb4
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-05-01 09:03:03 -0400

    removed old comments

--- RoadBuilder.java
+++ RoadBuilder.java
@@ -1,45 +1,18 @@
 public class RoadBuilder {
     public static void main(String[] args) {
         // set variables
-        int tileHeight = 20;
-        int tileWidth = 20;
-        int gridRows1 = 30;
-        int gridColumns1 = 30;
-        // int gridRows2 = 5;
-        // int gridColumns2 = 8;
+        int tileHeight = 25;
+        int tileWidth = 25;
+        int gridRows = 20;
+        int gridColumns = 30;
+        int[] goal = {1, 1};
+        int[] start = {1, 1};
+        int[][] obstacles = {};
+        int[][] keyTiles = {};
 
-        int[] goal1 = {1, 1};
-        // int[] goal2 = {6, 4};
-        // int[] goal3 = {6, 4};
-        int[] start1 = {1, 1};
-        // int[] start2 = {1, 1};
-        // int[] start3 = {1, 1};
+        Grid grid1 = new Grid(gridRows, gridColumns, makeCoords(obstacles), makeCoords(keyTiles), makeCoords(goal), makeCoords(start));
 
-        // // choose tiles to be obstacles
-        int[][] obstacles1 = {};
-        // int[][] obstacles2 = {{2, 1}, {5, 4}, {5, 3}, {3, 3}, {1, 3}, {3, 2}, {5, 2}};
-        // int[][] obstacles3 = {{2, 1}, {5, 2}, {3, 3}, {3, 4}, {5, 3}};
-
-        // // choose tiles to be keyTiles
-        int[][] keyTiles1 = {};
-        // int[][] keyTiles2 = {{3, 1}, {1, 4}};
-        // int[][] keyTiles3 = {{3, 1}, {1, 4}};
-
-        Grid grid1 = new Grid(gridRows1, gridColumns1, makeCoords(obstacles1), makeCoords(keyTiles1), makeCoords(goal1), makeCoords(start1));
-        // Grid grid2 = new Grid(gridRows1, gridColumns1, makeCoords(obstacles2), makeCoords(keyTiles2), makeCoords(goal2), makeCoords(start2));
-        // Grid grid3 = new Grid(gridRows2, gridColumns2, makeCoords(obstacles3), makeCoords(keyTiles3), makeCoords(goal3), makeCoords(start3));
-
-        // Grid grid = new Grid(30, 10, makeCoords(start), makeCoords(goal));
-        // visualize
-        // new Visualization("Dijkstras", grid, tileWidth, tileHeight);
         new Visualization("A*", grid1, tileWidth, tileHeight);
-        // new Visualization(grid2, pathfinder2.bfs_pathfinding(), tileWidth, tileHeight);
-        // new Visualization("A*", grid3, pathfinder3.aStar_pathfinding(), tileWidth, tileHeight);
-
-        // // visualize dijkstra
-        // vis.visualize(pathfinder.dijkstra_pathfinding());
-        // // visualize a*
-        // vis.visualize(pathfinder.aStar_pathfinding());
     }
 
     // helper functions to make it visually easier manually setting coords
